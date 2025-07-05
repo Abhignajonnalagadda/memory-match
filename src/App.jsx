01ef63337ff2e2e405b6d9be0ea8112ca1c1f0a0
LogeswariP <logeswari18082003@gmail.com>
2025-07-05 11:14:18 +0530

    Add Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Cards from './components/cards'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 className='bg-sky-500 hover:bg-sky-200'>Memory Match</h1>
+      <Cards/>
     </>
   )
 }
---
+++ src/components/cards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const cards = () => {
+  return (
+    <div className="rounded-sm border-2 p-2">45678</div> 
+  )
+}
+
+export default cards
