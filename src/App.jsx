cd10191e893bfc30d58189d3ee92c6fddf0ef8b0
LogeswariP <logeswari18082003@gmail.com>
2025-07-05 11:22:52 +0530

    rename cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Cards from './components/cards'
+import Card from './components/card'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Cards/>
+      <Card/>
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const cards = () => {
+const card = () => {
   return (
     <div className="rounded-sm border-2 p-2">45678</div> 
   )
 }
 
-export default cards
+export default card
